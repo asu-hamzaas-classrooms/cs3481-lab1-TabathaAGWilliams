c15e2bda2344e3989af6bbb692853dc3aaccc8b7
TabathaAGWilliams <geltta@appstate.edu>
2025-01-23 22:31:41 -0500

    finished copyBits(), passing all given tests

--- Tools.C
+++ Tools.C
@@ -206,7 +206,13 @@ uint64_t Tools::clearBits(uint64_t source, int32_t low, int32_t high)
 uint64_t Tools::copyBits(uint64_t source, uint64_t dest, 
                          int32_t srclow, int32_t dstlow, int32_t length)
 {
-   return 0; 
+  if (srclow < 0 || dstlow < 0 || srclow + length > 64 || dstlow + length > 64) {
+    return dest;
+  }
+  uint64_t extractedBits = getBits(source, srclow, (srclow + length - 1));
+  extractedBits = extractedBits << dstlow;
+  dest = clearBits(dest, dstlow, (dstlow + length - 1));
+  return dest | extractedBits; 
 }
 
 
